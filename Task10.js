1932578a131d660b56afbda1bc7ea845dac1d0f9
Balamuruganvelu <barathi5497@gmail.com>
2026-06-28 07:47:53 +0530

    TAsk 10

---
+++ Task10.js
@@ -0,0 +1,11 @@
+let product = {
+    name: "Laptop",
+    price: 50000,
+    brand: "ASUS",
+    stock: true
+};
+
+console.log("Product Name: " + product.name);
+console.log("Brand: " + product.brand);
+console.log("Price: " + product.price);
+console.log("Stock Available: " + product.stock);
