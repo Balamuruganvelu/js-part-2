27df949a4af00eb4c79cc6537b768e52bc86793f
Balamuruganvelu <barathi5497@gmail.com>
2026-06-28 07:20:55 +0530

    Task05

---
+++ Task05.js
@@ -0,0 +1,5 @@
+let cart = ["Rice", "Milk", "Sugar", "Tea Powder"];
+
+console.log("First Product: " + cart[0]);
+console.log("Last Product: " + cart[cart.length - 1]);
+console.log("Total Products: " + cart.length);
